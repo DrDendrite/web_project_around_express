3da7d3d172d75eda8071b47f0b90ac8535e08389
DrDendrite <ctpineda1@gmail.com>
2024-06-26 19:29:43 -0400

    api restful project 14

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
+const usersRouter = require("./routes/users");
+const cardRouter = require("./routes/cards");
+const mongoose = require("mongoose");
 const app = express();
 const { PORT = 3000 } = process.env;
 const {HttpStatus,HttpResponseMessage} = require("./enums/http")
 
-const usersRouter = require("./routes/users");
-const cardRouter = require("./routes/cards");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133'
+  };
+
+  next();
+});
+
+mongoose.connect('mongodb://localhost:27017/aroundb',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=> console.log("MongoDB connect successfully"))
+.catch(err=> console.error("Mongo connection error",err))
+
 
 app.use("/users", usersRouter);
 app.use("/cards", cardRouter);
-
 
 app.use((req, res) => {
   return res.status(HttpStatus.NOT_FOUND).send({message: HttpResponseMessage.NOT_FOUND});
